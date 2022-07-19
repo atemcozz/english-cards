780348042de14b7615aa70559468f6f77e1bf853
atemcozz <atemcozzartem@gmail.com>
2022-07-19 10:51:28 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: 0,
+      word: "",
+      translation: "",
+      cards: [],
+    };
+  }
+  updateInput(key, value) {
+    this.setState({
+      [key]: value,
+    });
+  }
+  addCard() {
+    const newCard = {
+      id: 1 + this.state.id,
+      value: {
+        word: this.state.word.slice(),
+        translation: this.state.translation.slice(),
+        flipped: false,
+      },
+    };
+    this.setState({
+      id: newCard.id,
+      word: "",
+      translation: "",
+      cards: [...this.state.cards, newCard],
+    });
+  }
+  turnCard(id) {
+    const cards = [...this.state.cards];
+    let index = cards.findIndex((card) => {
+      return card.id === id;
+    });
+    cards[index].value.flipped = !cards[index].value.flipped;
+    this.updateInput("cards", cards);
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1 className="app-title">Карточки английских слов</h1>
+        <div className="input__container">
+          Добавить карточку
+          <br />
+          <input
+            type="text"
+            placeholder="Введите слово"
+            value={this.state.word}
+            onChange={(event) => this.updateInput("word", event.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Введите слово"
+            value={this.state.translation}
+            onChange={(event) => {
+              this.updateInput("translation", event.target.value);
+            }}
+          />
+          <div
+            className="btn add-btn"
+            onClick={() => {
+              if (this.state.translation !== "" && this.state.word !== "")
+                this.addCard();
+            }}
+          >
+            Добавить
+          </div>
+        </div>
+        <div className="cards__container">
+          {this.state.cards.map((card) => {
+            return (
+              <div
+                key={card.id}
+                className={"card" + (card.value.flipped ? " flipped" : "")}
+                onClick={() => this.turnCard(card.id)}
+              >
+                {card.value.flipped ? card.value.translation : card.value.word}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
